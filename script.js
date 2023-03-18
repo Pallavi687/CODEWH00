7050d10d73afef70c4aa05dd506c8e6e120133be
Pallavi687 <pallavikhadse91@gmail.com>
2023-03-18 16:35:01 +0530

    main.html
    practice

--- script.js
+++ script.js
@@ -1,32 +1,71 @@
-function mydata()
-{
-    var day;
-    var day= prompt("Hello,Friends Enjoy Your day");
-    switch (new Date().getDay()) {
-        case 0:
-          day = "Sunday";
-          break;
-        case 1:
-          day = "Monday";
-          break;
-        case 2:
-          day = "Tuesday";
-          break;
-        case 3:
-          day = "Wednesday";
-          break;
-        case 4:
-          day = "Thursday";
-          break;
-        case 5:
-          day = "Friday";
-          break;
-        case  6:
-          day = "Saturday";
-          break;
-        default:
-          day = "Unknown Day";
-      }
-      document.getElementById("sample").innerHTML = "Today is " + day;
+// function mydata()
+// {
+//     var day;
+//     var day= prompt("Hello,Friends Enjoy Your day");
+//     switch (new Date().getDay()) {
+//         case 0:
+//           day = "Sunday";
+//           break;
+//         case 1:
+//           day = "Monday";
+//           break;
+//         case 2:
+//           day = "Tuesday";
+//           break;
+//         case 3:
+//           day = "Wednesday";
+//           break;
+//         case 4:
+//           day = "Thursday";
+//           break;
+//         case 5:
+//           day = "Friday";
+//           break;
+//         case  6:
+//           day = "Saturday";
+//           break;
+//         default:
+//           day = "Unknown Day";
+//       }
+//       document.getElementById("sample").innerHTML = "Today is " + day;
+//     }
+    // function factorial(n){
+    //   let answer = 1;
+    //   if (n == 0 || n == 1){
+    //     return answer;
+    //   }
+    //   else if(n > 1){
+    //     for(var i = n; i >= 1; i--){
+    //       answer = answer * i;
+    //     }
+    //     return answer;
+    //   }
+    //   else{
+    //     return "number has to be positive."
+    //   }  
+    // }
+    // let n=4;
+    // answer = factorial(n)
+    // console.log("Factorial of " + n + " : " + answer);
+  
+    // let num=prompt("please enter your number");
+    // let flag=0;
+    // for(let i=2; i<num;i++){
+    //   if(num%i==0){
+    //     console.log(num + "is not prime number.");
+    //     flag=1;
+    //   }
+    // }
+    // if(flag==0)
+    // {
+    //   console.log(num + "is prime number")
+    // }
+    
+    let a= prompt(!null);
+    if (a%2==0)
+    {
+      console.log("the number is even");
     }
-   
+    else{
+      console.log("The number is odd");
+    }
